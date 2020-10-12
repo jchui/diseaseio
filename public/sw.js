de3afb5df7329d1e78e4d1690e9adc9435b5a4be
Jeremy Chui <jeremy@jchui.me>
2020-10-12 23:49:32 +0100

    Logo update

--- public/sw.js
+++ public/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e9049ea4e53d2c1182b6.js"
+    "url": "webpack-runtime-3be1425ce51ddbe6d9f1.js"
   },
   {
     "url": "styles.b008d59f01631f665ac3.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6c3c04297b3d65027dbe2b83f002aa70"
+    "revision": "9bc327478d7905a838d8388b33018f6c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "58179afe90a40deb580640c70d4f1dcc"
+    "revision": "87861bee2fbdc08ac522223aee422ee0"
   },
   {
     "url": "polyfill-8d563e1c23881e77ed5b.js"
